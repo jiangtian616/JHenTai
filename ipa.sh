32b8f521c70cd964d0b50985e9a742e3c6f0d3b8
酱天小禽兽 <jiangtian616@qq.com>
2022-03-30 17:34:50 +0800

    ios 构建脚本
    ios build script

--- ipa.sh
+++ ipa.sh
@@ -1,1 +1,11 @@
 flutter build ios --release -t lib/src/main.dart
+
+mkdir ~/Desktop/Payload
+
+cp -r build/ios/Release-iphoneos/Runner.app/ ~/Desktop/Payload/Runner.app/
+
+cd ~/Desktop
+
+zip -ro JHenTai.ipa Payload
+
+rm -rf Payload
