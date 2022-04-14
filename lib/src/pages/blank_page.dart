0e5560416e24d8f3ea61541a2550ed684562842e
酱天小禽兽 <jiangtian616@qq.com>
2022-04-14 14:06:33 +0800

    修复空白页背景颜色未与主题同步的问题
    fix bug that color of blank page is not in sync with theme

--- lib/src/pages/blank_page.dart
+++ lib/src/pages/blank_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class BlankPage extends StatelessWidget {
   const BlankPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ColoredBox(color: Get.theme.backgroundColor);
+    return ColoredBox(color: Theme.of(context).backgroundColor);
   }
 }
