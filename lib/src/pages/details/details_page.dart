86f5e8b64bf36c80dddfe979b929255646cae0b0
酱天小禽兽 <jiangtian616@qq.com>
2022-07-16 18:20:46 +0800

    v2.1.4+50

--- lib/src/pages/details/details_page.dart
+++ lib/src/pages/details/details_page.dart
@@ -6,7 +6,6 @@ import 'package:flukit/flukit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
