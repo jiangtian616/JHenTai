955913ec146bd1034c2e7be530f2167901163881
酱天小禽兽 <jiangtian616@qq.com>
2022-07-16 12:08:51 +0800

    dmg.sh

--- dmg.sh
+++ dmg.sh
@@ -2,5 +2,5 @@ version=$(head -n 5 pubspec.yaml | tail -n 1 | cut -d ' ' -f 2)
 
 flutter build macos --release -t lib/src/main.dart \
 && cp -r build/macos/Build/Products/Release/jhentai.app ~/Desktop/jhentai.app \
-&& create-dmg --hdiutil-quiet ~/Desktop/JHenTai-${version}.dmg ~/Desktop/JHenTai/build/macos/Build/Products/Release/jhentai.app \
+&& create-dmg --hdiutil-quiet ~/Desktop/JHenTai-${version}.dmg ~/Desktop/jhentai.app \
 && rm -rf ~/Desktop/jhentai.app
