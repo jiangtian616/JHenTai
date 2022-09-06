20ff2472ec9fb694cc8b43a1eb7314dc22a62b60
酱天小禽兽 <jiangtian616@qq.com>
2022-09-06 23:34:18 +0800

    refactor advanced setting page

--- lib/src/pages/setting/advanced/setting_advanced_page.dart
+++ lib/src/pages/setting/advanced/setting_advanced_page.dart
@@ -4,6 +4,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:jhentai/src/extension/widget_extension.dart';
 import 'package:jhentai/src/network/eh_cache_interceptor.dart';
 import 'package:jhentai/src/setting/advanced_setting.dart';
 import 'package:jhentai/src/setting/path_setting.dart';
@@ -25,87 +26,93 @@ class _SettingAdvancedPageState extends State<SettingAdvancedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('advancedSetting'.tr),
-        elevation: 1,
-      ),
-      body: Obx(() {
-        return ListView(
-          physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+      appBar: AppBar(centerTitle: true, title: Text('advancedSetting'.tr)),
+      body: Obx(
+        () => ListView(
+          padding: const EdgeInsets.only(top: 16),
           children: [
-            ListTile(
-              title: Text('enableLogging'.tr),
-              trailing: Switch(
-                value: AdvancedSetting.enableLogging.value,
-                onChanged: (value) => AdvancedSetting.saveEnableLogging(value),
-              ),
-              subtitle: Text('needRestart'.tr),
-            ),
-            if (AdvancedSetting.enableLogging.isTrue)
-              FadeIn(
-                child: ListTile(
-                  title: Text('enableVerboseLogging'.tr),
-                  trailing: Switch(
-                    value: AdvancedSetting.enableVerboseLogging.value,
-                    onChanged: (value) => AdvancedSetting.saveEnableVerboseLogging(value),
-                  ),
-                ),
-              ),
-            ListTile(
-              title: Text('openLog'.tr),
-              trailing: const Icon(Icons.arrow_forward_ios, size: 20).marginOnly(right: 4),
-              onTap: () => toRoute(Routes.logList),
-            ),
+            _buildEnableLogging(),
+            if (AdvancedSetting.enableLogging.isTrue) _buildRecordAllLogs().fadeIn(),
 
-            /// Can't delete file that app is using on Windows
-            if (!GetPlatform.isWindows)
-              ListTile(
-                title: Text('clearLogs'.tr),
-                subtitle: Text('longPress2Clear'.tr),
-                trailing: Text(
-                  Log.getSize(),
-                  style: TextStyle(color: Get.theme.primaryColor, fontWeight: FontWeight.w500),
-                ).marginOnly(right: 8),
-                onLongPress: () {
-                  Log.clear();
-                  setState(() {
-                    toast('clearSuccess'.tr, isCenter: false);
-                  });
-                },
-              ),
-            ListTile(
-              title: Text('clearImagesCache'.tr),
-              subtitle: Text('longPress2Clear'.tr),
-              trailing: Text(
-                _getImagesCacheSize(),
-                style: TextStyle(color: Get.theme.primaryColor, fontWeight: FontWeight.w500),
-              ).marginOnly(right: 8),
-              onLongPress: () async {
-                await clearDiskCachedImages();
-                setState(() {
-                  toast('clearSuccess'.tr, isCenter: false);
-                });
-              },
-            ),
-            ListTile(
-              title: Text('clearPageCache'.tr),
-              subtitle: Text('longPress2Clear'.tr),
-              onLongPress: () async {
-                await Get.find<EHCacheInterceptor>().removeAllCache();
-                toast('clearSuccess'.tr, isCenter: false);
-              },
-            ),
-            ListTile(
-              title: Text('checkUpdateAfterLaunchingApp'.tr),
-              trailing: Switch(
-                value: AdvancedSetting.enableCheckUpdate.value,
-                onChanged: (value) => AdvancedSetting.saveEnableCheckUpdate(value),
-              ),
-            ),
+            /// Can't delete file that file is being used on Windows
+            if (!GetPlatform.isWindows) _buildClearLogs(),
+            _buildOpenLogs(),
+            _buildClearLogs(),
+            _buildClearImageCache(),
+            _buildClearNetworkCache(),
+            _buildCheckUpdate(),
           ],
-        ).paddingSymmetric(vertical: 16);
-      }),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEnableLogging() {
+    return ListTile(
+      title: Text('enableLogging'.tr),
+      trailing: Switch(value: AdvancedSetting.enableLogging.value, onChanged: AdvancedSetting.saveEnableLogging),
+      subtitle: Text('needRestart'.tr),
+    );
+  }
+
+  Widget _buildRecordAllLogs() {
+    return ListTile(
+      title: Text('enableVerboseLogging'.tr),
+      trailing: Switch(value: AdvancedSetting.enableVerboseLogging.value, onChanged: AdvancedSetting.saveEnableVerboseLogging),
+    );
+  }
+
+  Widget _buildOpenLogs() {
+    return ListTile(
+      title: Text('openLog'.tr),
+      trailing: const Icon(Icons.keyboard_arrow_right).marginOnly(right: 4),
+      onTap: () => toRoute(Routes.logList),
+    );
+  }
+
+  Widget _buildClearLogs() {
+    return ListTile(
+      title: Text('clearLogs'.tr),
+      subtitle: Text('longPress2Clear'.tr),
+      trailing: Text(Log.getSize(), style: TextStyle(color: Get.theme.primaryColor, fontWeight: FontWeight.w500)).marginOnly(right: 8),
+      onLongPress: () {
+        Log.clear();
+        setState(() {
+          toast('clearSuccess'.tr, isCenter: false);
+        });
+      },
+    );
+  }
+
+  Widget _buildClearImageCache() {
+    return ListTile(
+      title: Text('clearImagesCache'.tr),
+      subtitle: Text('longPress2Clear'.tr),
+      trailing: Text(_getImagesCacheSize(), style: TextStyle(color: Get.theme.primaryColor, fontWeight: FontWeight.w500)).marginOnly(right: 8),
+      onLongPress: () async {
+        await clearDiskCachedImages();
+        setState(() {
+          toast('clearSuccess'.tr, isCenter: false);
+        });
+      },
+    );
+  }
+
+  Widget _buildClearNetworkCache() {
+    return ListTile(
+      title: Text('clearPageCache'.tr),
+      subtitle: Text('longPress2Clear'.tr),
+      onLongPress: () async {
+        await Get.find<EHCacheInterceptor>().removeAllCache();
+        toast('clearSuccess'.tr, isCenter: false);
+      },
+    );
+  }
+
+  Widget _buildCheckUpdate() {
+    return ListTile(
+      title: Text('checkUpdateAfterLaunchingApp'.tr),
+      trailing: Switch(value: AdvancedSetting.enableCheckUpdate.value, onChanged: AdvancedSetting.saveEnableCheckUpdate),
     );
   }
 
