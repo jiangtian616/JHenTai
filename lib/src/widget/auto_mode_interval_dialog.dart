f58176bff85f77fdd2f5abe222c49bc1a77e7efe
酱天小禽兽 <jiangtian616@qq.com>
2022-09-13 22:35:31 +0800

    Fix bug with updating gallery, the option of whether to download the original image is not the same as the original gallery. Fix bug with crash when load My Tags failed.

--- lib/src/widget/auto_mode_interval_dialog.dart
+++ lib/src/widget/auto_mode_interval_dialog.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jhentai/src/setting/read_setting.dart';
 
+import '../config/ui_config.dart';
 import '../utils/route_util.dart';
 
 class AutoModeIntervalDialog extends StatefulWidget {
@@ -14,35 +15,40 @@ class AutoModeIntervalDialog extends StatefulWidget {
 
 class _AutoModeIntervalDialogState extends State<AutoModeIntervalDialog> {
   double interval = ReadSetting.autoModeInterval.value;
+  FixedExtentScrollController scrollController = FixedExtentScrollController(initialItem: ReadSetting.autoModeInterval.value ~/ 0.5 - 1);
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      contentPadding: const EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 6),
-      actionsPadding: const EdgeInsets.only(left: 16, right: 16, bottom: 8),
-      title: Text('autoModeInterval'.tr),
-      content: SizedBox(
-        height: 150,
-        child: CupertinoPicker.builder(
-          itemExtent: 30,
-          onSelectedItemChanged: (index) => interval = (index + 1) * 0.5,
-          scrollController: FixedExtentScrollController(initialItem: interval ~/ 0.5 - 1),
-          itemBuilder: (_, int index) => Center(
-            child: Text('${(index + 1) * 0.5} s', style: TextStyle(color: Get.theme.colorScheme.onBackground)),
+    return ScrollConfiguration(
+      behavior: UIConfig.behaviorWithoutScrollBar,
+      child: AlertDialog(
+        contentPadding: const EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 6),
+        actionsPadding: const EdgeInsets.only(left: 16, right: 16, bottom: 8),
+        title: Text('autoModeInterval'.tr),
+        content: SizedBox(
+          height: 150,
+          child: CupertinoPicker.builder(
+            itemExtent: 30,
+            onSelectedItemChanged: (index) => interval = (index + 1) * 0.5,
+            scrollController: scrollController,
+            itemBuilder: (_, int index) => Center(
+              child: Text('${(index + 1) * 0.5} s', style: TextStyle(color: Get.theme.colorScheme.onBackground)),
+            ),
+            childCount: 39,
           ),
-          childCount: 39,
         ),
+        actions: [
+          TextButton(child: Text('cancel'.tr), onPressed: backRoute),
+          TextButton(
+            child: Text('OK'.tr),
+            onPressed: () {
+              ReadSetting.saveAutoModeInterval(interval);
+              backRoute(result: true);
+            },
+          ),
+        ],
       ),
-      actions: [
-        TextButton(child: Text('cancel'.tr), onPressed: backRoute),
-        TextButton(
-          child: Text('OK'.tr),
-          onPressed: () {
-            ReadSetting.saveAutoModeInterval(interval);
-            backRoute(result: true);
-          },
-        ),
-      ],
     );
+
   }
 }
