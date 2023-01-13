fc43aebd8275661cad342d0a251919de4c804929
酱天小禽兽 <jiangtian616@qq.com>
2023-01-13 23:48:27 +0800

    v6.3.1+112

--- lib/src/setting/frame_rate_setting.dart
+++ lib/src/setting/frame_rate_setting.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
+import 'package:get/get.dart';
 
 import '../utils/log.dart';
 
 class FrameRateSetting {
   static Future<void> init() async {
+    if (!GetPlatform.isAndroid) {
+      return;
+    }
     try {
       await FlutterDisplayMode.setHighRefreshRate();
     } on PlatformException catch (e) {
