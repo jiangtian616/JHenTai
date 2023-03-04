61856273d851881cbc1c77661706563bdd301062
Luca Park <qlife1146@gmail.com>
2023-03-05 06:59:43 +0900

    add korean translation.

--- lib/src/l18n/locale_text.dart
+++ lib/src/l18n/locale_text.dart
@@ -3,6 +3,7 @@ import 'package:jhentai/src/l18n/en_US.dart';
 import 'package:jhentai/src/l18n/zh_CN.dart';
 import 'package:jhentai/src/l18n/zh_TW.dart';
 import 'package:jhentai/src/l18n/pt_BR.dart';
+import 'package:jhentai/src/l18n/ko_KR.dart';
 
 class LocaleText extends Translations {
   @override
@@ -11,5 +12,6 @@ class LocaleText extends Translations {
         'zh_CN': zh_CN.keys(),
         'zh_TW': zh_TW.keys(),
         'pt_BR': pt_BR.keys(),
+        'ko_KR': ko_KR.keys(),
       };
 }
