b22bc84be3c5c8978d9e2436c96c5ab34dc11278
酱天小禽兽 <jiangtian616@qq.com>
2023-03-21 17:47:30 +0800

    Distribute .pkg file for macOS macOS安装包格式改为.pkg

--- dmg.sh
+++ dmg.sh
@@ -1,6 +1,8 @@
 version=$(head -n 5 pubspec.yaml | tail -n 1 | cut -d ' ' -f 2)
 
 flutter build macos --release -t lib/src/main.dart \
-&& cp -r build/macos/Build/Products/Release/jhentai.app ~/Desktop/jhentai.app \
-&& create-dmg --hdiutil-quiet ~/Desktop/JHenTai-${version}.dmg ~/Desktop/jhentai.app \
-&& rm -rf ~/Desktop/jhentai.app
+&& hdiutil create -size 150m -fs HFS+ -volname JHenTai JHenTai.dmg \
+&& hdiutil attach JHenTai.dmg \
+&& cp -R build/macos/Build/Products/Release/jhentai.app /Volumes/JHenTai \
+&& pkgbuild --install-location /Applications/JHenTai.app --identifier top.jtmonster.jhentai --version ${version} --root /Volumes/JHenTai/jhentai.app build/macos/JHenTai.pkg \
+&& hdiutil detach /Volumes/JHenTai
