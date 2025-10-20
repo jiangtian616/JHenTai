3f3ff3a1a13e17cfa63bf6499d70fe1f6770f622
Tokinogaki <Tokinogaki@gmail.com>
2025-10-20 19:54:13 +0800

    fix bug

--- lib/src/service/ml_tts_service.dart
+++ lib/src/service/ml_tts_service.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:extended_image/extended_image.dart';
 import 'package:get/get_rx/src/rx_workers/rx_workers.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:jhentai/src/setting/read_setting.dart';
 import 'package:jhentai/src/service/log.dart';
 
@@ -15,56 +14,9 @@ MlttsService mlTtsService = MlttsService();
 enum TtsState { playing, stopped, paused, continued }
 
 class MlttsService with JHLifeCircleBeanErrorCatch implements JHLifeCircleBean {
-  // latin: 拉丁文, chinese：汉文, devanagiri：梵文, japanese：日文, korean：韩文,
-  /* 1. 拉丁文（使用拉丁字母的语言）：
-ca-ES（加泰罗尼亚语）
-nl-NL（荷兰语）
-fr-CA（法语）
-en-AU（英语）
-id-ID（印尼语）
-da-DK（丹麦语）
-sk-SK（斯洛伐克语）
-sv-SE（瑞典语）
-tr-TR（土耳其语）
-pt-BR（葡萄牙语）
-es-MX（西班牙语）
-en-US（英语）
-hu-HU（匈牙利语）
-fr-FR（法语）
-pt-PT（葡萄牙语）
-hr-HR（克罗地亚语）
-en-GB（英语）
-nl-BE（荷兰语）
-pl-PL（波兰语）
-vi-VN（越南语）
-de-DE（德语）
-fi-FI（芬兰语）
-es-ES（西班牙语）
-ms-MY（马来语）
-en-IE（英语）
-sl-SI（斯洛文尼亚语）
-en-IN（英语）
-it-IT（意大利语）
-ro-RO（罗马尼亚语）
-nb-NO（挪威博克马尔语）
-en-ZA（英语）
-cs-CZ（捷克语）
-2. 汉文（使用汉字的语言）：
-zh-CN（中文）
-zh-HK（中文）
-zh-TW（中文）
-3. 梵文（使用天城文的语言）：
-hi-IN（印地语）
-4. 日文（使用日文汉字和假名的语言）：
-ja-JP（日语）
-
-5. 韩文（使用谚文的语言）：
-ko-KR（韩语）
-其他代码：
-列表中的其他代码（如 ar-001、uk-UA、th-TH、he-IL、el-GR、ru-RU、bg-BG 等）使用阿拉伯字母、西里尔字母、泰文字母、希伯来字母、希腊字母等文字系统，不属于您提到的五种语种范围。
-  */
   late TextRecognizer _textRecognizer;
   String _extractedText = '';
+  List<String> _exclusionList = [];
 
   String? _path;
   late FlutterTts _flutterTts;
@@ -84,6 +36,7 @@ ko-KR（韩语）
   late Worker mlTtsPitchLister;
   late Worker mlTtsRateLister;
   late Worker mlTtsVolumeLister;
+  late Worker mlTtsExclusionListLister;
 
   Future<void> _initConfig(_) async {
     await _flutterTts.setVolume(readSetting.mlTtsVolume.value);
@@ -91,6 +44,9 @@ ko-KR（韩语）
     await _flutterTts.setPitch(readSetting.mlTtsPitch.value);
     await _flutterTts.setLanguage(readSetting.mlTtsLanguage.value!);
     await _setEngine();
+    _addListers();
+    _setLanguages();
+    _setExclusionList();
 
     _flutterTts.setStartHandler(() {
       log.debug('setStartHandler:Playing');
@@ -123,6 +79,14 @@ ko-KR（韩语）
     });
   }
 
+  Future<void> _setLanguages() async {
+    var langs = await _flutterTts.getLanguages;
+    for (var element in langs) {
+      languages.add(element);
+    }
+    log.debug('doAfterBeanReady: $languages');
+  }
+
   Future<void> _setEngine() async {
     if (Platform.isAndroid) {
       engines = [];
@@ -149,10 +113,10 @@ ko-KR（韩语）
     }
   }
 
-  List<String> _getExclusionList() {
+  void _setExclusionList() {
     var exclusionList = readSetting.mlTtsExclusionList.value?.split(',') ?? [];
     exclusionList = exclusionList.map((e) => e.trim()).toList();
-    return exclusionList.where((e) => e != '').toList();
+    _exclusionList = exclusionList.where((e) => e != '').toList();
   }
 
   void dispose() {
@@ -162,6 +126,7 @@ ko-KR（韩语）
     mlTtsPitchLister.dispose();
     mlTtsRateLister.dispose();
     mlTtsVolumeLister.dispose();
+    mlTtsExclusionListLister.dispose();
   }
 
   void _addListers() {
@@ -183,6 +148,9 @@ ko-KR（韩语）
     mlTtsVolumeLister = ever(readSetting.mlTtsVolume, (_) {
       _flutterTts.setVolume(readSetting.mlTtsVolume.value);
     });
+    mlTtsExclusionListLister = ever(readSetting.mlTtsExclusionList, (_) {
+      _setExclusionList();
+    });
   }
 
   @override
@@ -194,23 +162,19 @@ ko-KR（韩语）
   }
 
   @override
-  Future<void> doAfterBeanReady() async {
-    _addListers();
-    var langs = await _flutterTts.getLanguages;
-    for (var element in langs) {
-      languages.add(element);
-    }
-    log.debug('doAfterBeanReady: $languages');
+  Future<void> doAfterBeanReady() {
+    throw UnimplementedError();
   }
 
   Future<void> _recognizedText() async {
     try {
       final inputImage = InputImage.fromFilePath(_path!);
-      final RecognizedText text = await _textRecognizer.processImage(inputImage);
+      final RecognizedText text =
+          await _textRecognizer.processImage(inputImage);
       _extractedText = '';
       for (var block in text.blocks) {
         var t = block.text.replaceAll(RegExp(r'\s'), '');
-        for (var val in _getExclusionList()) {
+        for (var val in _exclusionList) {
           if (t.toLowerCase().contains(val)) {
             continue;
           }
@@ -272,4 +236,5 @@ ko-KR（韩语）
       ttsState = TtsState.paused;
     }
   }
+  
 }
