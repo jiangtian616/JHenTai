b3ee93bef11539963fb59e5fac2661f8a360938c
Wen_He <wen_he_@outlook.com>
2026-03-17 23:51:54 +0800

    fix(aria2): import cupertino for test connection indicator

--- lib/src/pages/setting/download/aria2/aria2_settings_page.dart
+++ lib/src/pages/setting/download/aria2/aria2_settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jhentai/src/extension/widget_extension.dart';
